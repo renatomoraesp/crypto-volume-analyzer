996cf5f2e467e09ec37855dc497d4528c2c199ff
renatomoraesp <renatomoraespinheiro@gmail.com>
2021-03-02 17:03:15 -0300

    added try and catch blocks to async function

--- api.js
+++ api.js
@@ -23,82 +23,90 @@ const exchangeId = 'binance'
 
 
 const getVolumes = async () => {
-    const tickers = await binance.fetchTickers();
-    let count = 0;
-    let tempVolume, tempPrice;
-    if (masterBool) {
-        for (const [key, value] of Object.entries(tickers)) {
-            if (value.baseVolume === 0 || volumeInfo[[key]].volumes[0] === 0) {
+    try {
+        const tickers = await binance.fetchTickers();
+        let count = 0;
+        let tempVolume, tempPrice;
+        if (masterBool) {
+            for (const [key, value] of Object.entries(tickers)) {
+                if (value.baseVolume === 0 || volumeInfo[[key]].volumes[0] === 0) {
+                    count++;
+                    continue;
+                }
+                tempVolume = volumeInfo[[key]].volumes[0];
+                tempPrice = volumeInfo[[key]]['closing prices'][0];
+                if (value.baseVolume / tempVolume < 2) {
+                    volumeInfo[[key]] = {
+                        'volumes': [value.baseVolume, tempVolume],
+                        'pump index': [value.baseVolume / tempVolume],
+                        'closing prices': [value.close, tempPrice]
+                    };
+                }
                 count++;
-                continue;
             }
-            tempVolume = volumeInfo[[key]].volumes[0];
-            tempPrice = volumeInfo[[key]]['closing prices'][0];
-            if (value.baseVolume / tempVolume < 2) {
+        } else {
+            for (const [key, value] of Object.entries(tickers)) {
                 volumeInfo[[key]] = {
-                    'volumes': [value.baseVolume, tempVolume],
-                    'pump index': [value.baseVolume / tempVolume],
-                    'closing prices': [value.close, tempPrice]
+                    'volumes': [value.baseVolume, 0],
+                    'pump index': [0],
+                    'closing prices': [value.close, 0]
                 };
+                count++;
             }
-            count++;
+            masterBool = true;
         }
-    } else {
-        for (const [key, value] of Object.entries(tickers)) {
-            volumeInfo[[key]] = {
-                'volumes': [value.baseVolume, 0],
-                'pump index': [0],
-                'closing prices': [value.close, 0]
-            };
-            count++;
-        }
-        masterBool = true;
+    } catch(err) {
+        console.log('ERROR\n', err);
     }
 }
 
 
 const getRanking = async () => {
 
-    await binance.loadMarkets();
-    await getVolumes();
-    const alertArray = [];
+    try {
+        await binance.loadMarkets();
+        await getVolumes();
+        const alertArray = [];
 
-    const tempArray = [];
-    for (let property in volumeInfo) {
-        tempArray.push({ [property]: volumeInfo[property] });
-    }
-    tempArray.sort((a, b) => (a[Object.keys(a)[0]]['pump index'] < b[Object.keys(b)[0]]['pump index']) ? 1 : ((b[Object.keys(b)[0]]['pump index'] < a[Object.keys(a)[0]]['pump index']) ? -1 : 0));
+        const tempArray = [];
+        for (let property in volumeInfo) {
+            tempArray.push({ [property]: volumeInfo[property] });
+        }
+        tempArray.sort((a, b) => (a[Object.keys(a)[0]]['pump index'] < b[Object.keys(b)[0]]['pump index']) ? 1 : ((b[Object.keys(b)[0]]['pump index'] < a[Object.keys(a)[0]]['pump index']) ? -1 : 0));
 
-    if (servantBool) {
-        let tempIndex = 0;
-        for (let i = 0; i < 30; i++) {
-            for (let j = tempIndex; j < 30; j++) {
-                if (ranking[i][Object.keys(ranking[i])[0]]['pump index'] < tempArray[j][Object.keys(tempArray[j])[0]]['pump index']) {
-                    ranking.splice(i, 0, tempArray[j]);
-                    ranking.pop();
+        if (servantBool) {
+            let tempIndex = 0;
+            for (let i = 0; i < 30; i++) {
+                for (let j = tempIndex; j < 30; j++) {
+                    if (ranking[i][Object.keys(ranking[i])[0]]['pump index'] < tempArray[j][Object.keys(tempArray[j])[0]]['pump index']) {
+                        ranking.splice(i, 0, tempArray[j]);
+                        ranking.pop();
 
-                    if (tempArray[j][Object.keys(tempArray[j])[0]]['closing prices'][0] != tempArray[j][Object.keys(tempArray[j])[0]]['closing prices'][1] && ranking[i][Object.keys(ranking[i])[0]]['pump index'] > 1.1) {
-                        alertArray.push({
-                            'pair': Object.keys(tempArray[j])[0],
-                            'pump index': ((ranking[i][Object.keys(ranking[i])[0]]['pump index'] - 1) * 100).toFixed(2).toString() + '%',
-                            'price increase': (((tempArray[j][Object.keys(tempArray[j])[0]]['closing prices'][0] / tempArray[j][Object.keys(tempArray[j])[0]]['closing prices'][1]) - 1) * 100).toFixed(2).toString()
-                        });
+                        if (tempArray[j][Object.keys(tempArray[j])[0]]['closing prices'][0] != tempArray[j][Object.keys(tempArray[j])[0]]['closing prices'][1] && ranking[i][Object.keys(ranking[i])[0]]['pump index'] > 1.1) {
+                            alertArray.push({
+                                'pair': Object.keys(tempArray[j])[0],
+                                'pump index': ((ranking[i][Object.keys(ranking[i])[0]]['pump index'] - 1) * 100).toFixed(2).toString() + '%',
+                                'price increase': (((tempArray[j][Object.keys(tempArray[j])[0]]['closing prices'][0] / tempArray[j][Object.keys(tempArray[j])[0]]['closing prices'][1]) - 1) * 100).toFixed(2).toString()
+                            });
+                        }
+
+                        tempIndex = j + 1;
+                        break;
                     }
-
-                    tempIndex = j + 1;
-                    break;
                 }
+                if (tempIndex == 29) break;
             }
-            if (tempIndex == 29) break;
+        } else {
+            for (let i = 0; i < 30; i++) {
+                ranking.push(tempArray[i]);
+            }
+            servantBool = true;
         }
-    } else {
-        for (let i = 0; i < 30; i++) {
-            ranking.push(tempArray[i]);
-        }
-        servantBool = true;
-    }
 
-    return [ranking, alertArray];
+        return [ranking, alertArray];
+    } catch(err) {
+        console.log('ERROR\n', err);
+    }
 }
 
 module.exports = {
